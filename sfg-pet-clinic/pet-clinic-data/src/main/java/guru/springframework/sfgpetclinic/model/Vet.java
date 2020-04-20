ef9644dadcf921f493fdea175a24d9c3b14c0b21
Eri <Eri@Ertugert-Pc>
2020-04-20 16:56:48 +0200

    added speciality ,added to vet.closes #27

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet  extends Person{
 
 	/**
@@ -7,4 +9,16 @@ public class Vet  extends Person{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Set<Speciality> speciality;
+
+	public Set<Speciality> getSpeciality() {
+		return speciality;
+	}
+
+	public void setSpeciality(Set<Speciality> speciality) {
+		this.speciality = speciality;
+	}
+	
+	
+	
 }
